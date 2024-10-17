6a852f60ee500575bbb706a28e402b5acb6d7dfd
Шпилева Анастасия <62290933+banka-lecho@users.noreply.github.com>
2024-10-17 14:47:33 +0300

    Update feature_extraction.py

--- lab5/feature_extraction.py
+++ lab5/feature_extraction.py
@@ -14,15 +14,10 @@ class WrapperFeatureSelector:
     def get_best_feature(self, X, y, selected_features):
         best_feature = None
         max_quality = 0
-        # Для каждого признака в features создается поднабор X_subset, который содержит уже выбранные
-        # признаки (selected_features) плюс текущий признак (feature).
         for feature in self.features:
             X_subset = X[selected_features + [feature]]
-            # Оценка качества модели происходит с помощью функции f1_score
             self.classifier.fit(X_subset, y)
             quality = f1_score(y, self.classifier.predict(X_subset))
-            # Если текущая F1-мера превышает максимальную из уже найденных (max_quality), обновляются как
-            # максимальная мера, так и лучший признак.
             if quality > max_quality:
                 max_quality = quality
                 best_feature = feature
@@ -31,11 +26,8 @@ class WrapperFeatureSelector:
     def select_features(self, X, y, n_features):
         selected_features = []
         self.features = set(X.columns.values)
-        # На каждой итерации производится поиск наилучшего признака
         for i in range(n_features):
             best_feature = self.get_best_feature(X, y, selected_features)
-            # После завершения внутреннего цикла (for feature in features) наилучший признак добавляется в
-            # selected_features, и удаляется из features.
             selected_features.append(best_feature)
             self.features.remove(best_feature)
         return selected_features
@@ -74,25 +66,13 @@ class FilterFeatureSelector(BaseEstimator):
     def mutual_information(self, X, y):
         """
         Вычисляет взаимную информацию между признаками X и целевой переменной y.
-
-        Параметры:
-        X (numpy.ndarray) - матрица признаков
-        y (numpy.ndarray) - вектор целевой переменной
-
-        Возвращает:
-        numpy.ndarray - вектор взаимной информации для каждого признака
         """
         n_features = X.shape[1]
         self.mutual_infos = np.zeros(n_features)
         for i in range(n_features):
-            # Вычисляем энтропию признака
             feature_entropy = entropy(np.unique(X[:, i], return_counts=True)[1])
-
-            # Вычисляем условную энтропию признака относительно целевой переменной
             feature_conditional_entropy = np.mean(
                 [entropy(np.unique(X[y == cls, i], return_counts=True)[1]) for cls in np.unique(y)])
-
-            # Вычисляем взаимную информацию
             self.mutual_infos[i] = feature_entropy - feature_conditional_entropy
 
         return self.mutual_infos
