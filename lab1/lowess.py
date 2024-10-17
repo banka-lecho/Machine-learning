83c70eee898631e903ba52f982aff5a126ef6da8
Шпилева Анастасия <62290933+banka-lecho@users.noreply.github.com>
2024-10-17 15:53:18 +0300

    Update lowess.py

--- lab1/lowess.py
+++ lab1/lowess.py
@@ -27,12 +27,8 @@ class Regression_LOWESS:
         for _ in range(self.n_iterations):
             predictions = beta_0 + beta_1 * x
             errors = predictions - y
-
-            # Вычисляем градиенты, взвешенные по весам
             gradient_0 = (1 / m) * np.sum(weights * errors)
             gradient_1 = (1 / m) * np.sum(weights * errors * x)
-
-            # Обновление параметров
             beta_0 -= self.learning_rate * gradient_0
             beta_1 -= self.learning_rate * gradient_1
 
@@ -52,24 +48,16 @@ class Regression_LOWESS:
         self.lowess_weights = [0] * n
         r = int(self.f * n)
         for i in tqdm(range(n), desc="Обучение lOWESS"):
-            # Вычисляем расстояния от текущего x до всех остальных
             distances = np.abs(x - x[i])
-            # Получаем индексы определенного количества соседей
             idx = np.argsort(distances)[:r]
-            # Извлекаем x и y для соседей
             x_neighbors = x[idx]
             y_neighbors = y[idx]
             dists_neighbours = distances[idx]
-            # Вычисляем веса каждого соседа для объекта
             weights = self.tricube_weight(dists_neighbours / dists_neighbours.max())
-            # теперь чтобы достать это в knn, мне надо для каждого объекта положить в этот массив таргет
-            # и веса для этого объекта
             self.lowess_weights[i] = (list(y_neighbors), list(weights))
             if self.decision_method == 'gradient_descent':
-                # находим взвешенную линейную регрессию с помощью ГС
                 beta_0, beta_1 = self.gradient_descent(x_neighbors, y_neighbors, weights)
             else:
-                # находим взвешенную линейную регрессию с помощью аналитического метода
                 beta_0, beta_1 = self.analytical_decision(x_neighbors, y_neighbors, weights)
             smoothed[i] = beta_0 + beta_1 * x[i]
         return smoothed
